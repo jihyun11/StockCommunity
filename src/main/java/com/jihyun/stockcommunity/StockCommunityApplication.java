c04be2c9214da92fd6cf492fcad9fee4d769ca05
jihyun <372491@naver.com>
2023-05-27 11:03:51 +0900

    StockCommunity

---
+++ src/main/java/com/jihyun/stockcommunity/StockCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.jihyun.stockcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockCommunityApplication.class, args);
+    }
+
+}

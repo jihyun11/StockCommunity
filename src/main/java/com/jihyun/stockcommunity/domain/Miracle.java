19123f3425f7c46414d1c7893abebc08377d7165
jihyun <372491@naver.com>
2023-08-29 16:23:46 +0900

    소원 페이지 도메인 구성

---
+++ src/main/java/com/jihyun/stockcommunity/domain/Miracle.java
@@ -0,0 +1,14 @@
+package com.jihyun.stockcommunity.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Miracle {
+
+    private String miracle_id;
+    private String miracle;
+    private String miracle_date;
+
+}
